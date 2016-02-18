a1c910bfac9df943583d6926b6ee81f761e1c0d5
Geir Gåsodden <geir.gasodden@pythonia.no>
2016-02-18 15:28:08 +0100

    Returns false instead of error if none found

--- index.js
+++ index.js
@@ -4,16 +4,21 @@ function unwrapContact (contact) {
   if (!contact) {
     throw new Error('Missing required input: contact object')
   }
+  var person = false
 
-  if (contact.GetPrivatePersonsResult && contact.GetPrivatePersonsResult.PrivatePersons && contact.GetPrivatePersonsResult.PrivatePersons.PrivatePersonBase) {
-    if (contact.GetPrivatePersonsResult.PrivatePersons.PrivatePersonBase.length === 1) {
-      return contact.GetPrivatePersonsResult.PrivatePersons.PrivatePersonBase[0]
-    } else {
-      throw new Error('More than 1 person in contact object')
+  if (contact.GetPrivatePersonsResult && contact.GetPrivatePersonsResult.PrivatePersons) {
+    if (contact.GetPrivatePersonsResult.PrivatePersons.PrivatePersonBase) {
+      if (contact.GetPrivatePersonsResult.PrivatePersons.PrivatePersonBase.length === 1) {
+        person = contact.GetPrivatePersonsResult.PrivatePersons.PrivatePersonBase[0]
+      } else {
+        throw new Error('More than 1 person in contact object')
+      }
     }
   } else {
     throw new Error('Malformed contact object')
   }
+
+  return person
 }
 
 module.exports = unwrapContact
--- test/tfk-360-unwrap-contact-test.js
+++ test/tfk-360-unwrap-contact-test.js
@@ -29,10 +29,18 @@ tap.throws(
   'Throws if contact object is malformed'
 )
 
+tap.test('It returns false if no person found', function (test) {
+  var contact = require('./data/GetPrivatePersonsResultNoneFound.json')
+  var expected = false
+  var result = unwrapContact(contact)
+  tap.equal(JSON.stringify(result), JSON.stringify(expected), 'Contact returned false OK')
+  test.done()
+})
+
 tap.test('It returns expected result', function (test) {
   var contact = require('./data/GetPrivatePersonsResult.json')
   var expected = require('./data/unwrappedContact.json')
   var result = unwrapContact(contact)
-  tap.equal(result.toString(), expected.toString(), 'Contact unwrapped OK')
+  tap.equal(JSON.stringify(result), JSON.stringify(expected), 'Contact unwrapped OK')
   test.done()
 })
